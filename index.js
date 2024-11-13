6381b4f3fab6902229de7c349fa4342bbb7ca70a
Anantadhirya <anantadhirya@gmail.com>
2024-11-13 12:27:45 +0700

    feat: errorHandler

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const errorHandler = require("./src/middleware/errorHandler");
+
 dotenv.config();
 
 const app = express();
@@ -17,6 +19,8 @@ app.use(morgan("dev"));
 app.route("/").get((req, res, next) => {
   res.status(200).json({ message: "Hello World" });
 });
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
