e419ba8e5e88406a5faf957efa48c1dfba008884
Wafi-Afdi <wafiafdi@gmail.com>
2024-11-26 16:58:59 +0700

    test origin true

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -5,19 +5,7 @@ const corsConfig = () => {
     if(process.env.environment == "production") {
         let allowedOrigins = [process.env.FE_DOMAIN];
         return {
-            origin: function(origin, callback){
-                // allow requests with no origin 
-                // (like mobile apps or curl requests)
-                if(!origin) return callback(null, true);
-
-
-                if(allowedOrigins.indexOf(origin) === -1){
-                    var msg = 'The CORS policy for this site does not ' +
-                            'allow access from the specified Origin.';
-                    return callback(new Error(msg), false);
-                }
-                return callback(null, true);
-            } ,
+            origin:true,
             credentials :true
         }
     } else {
