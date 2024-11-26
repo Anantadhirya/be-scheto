495f1b6c01a1524a5dc3d6cc0ee866ae288e0af7
Wafi-Afdi <wafiafdi@gmail.com>
2024-11-26 21:34:44 +0700

    update : jadikan lax

--- src/config/cookieConfig.js
+++ src/config/cookieConfig.js
@@ -2,7 +2,7 @@ const cookieConfig = {
     maxAge : 1000 * 60 * 60 * 24,
     httpOnly : true,
     secure : process.env.environment == "development" ? false : true,
-    sameSite : process.env.environment == "development" ?'lax' : 'none',
+    sameSite : process.env.environment == "development" ?'lax' : 'lax',
     domain : process.env.FE_DOMAIN,
 }
 
