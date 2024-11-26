8f2a38a053415d2064d462c7bfc29cf2d60f0394
Anantadhirya <anantadhirya@gmail.com>
2024-11-26 18:57:52 +0700

    fix: cors

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser")
-const mongoose = require("mongoose")
-const helmet = require("helmet")
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
 
 const errorHandler = require("./src/middleware/errorHandler");
-const rateLimiter = require("./src/middleware/rateLimiter")
+const rateLimiter = require("./src/middleware/rateLimiter");
 
 dotenv.config();
 
 const app = express();
 
-const corsConfig = require('./src/config/corsConfig')
-app.use(cors(corsConfig()));
+const corsConfig = require("./src/config/corsConfig");
+app.use(cors({ origin: true, credentials: true }));
 
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(rateLimiter.rateLimiterStandard)
+app.use(rateLimiter.rateLimiterStandard);
 app.use(helmet.xssFilter());
 
 // Routes
@@ -28,29 +28,26 @@ app.route("/").get((req, res, next) => {
   res.status(200).json({ message: "Hello World" });
 });
 
-app.use("/auth", require("./src/routes/authRoutes") )
-app.use("/profile", require("./src/routes/profileRoutes") )
-app.use("/group", require("./src/routes/groupRoutes") )
-app.use("/schedule", require("./src/routes/scheduleRoutes") )
-
+app.use("/auth", require("./src/routes/authRoutes"));
+app.use("/profile", require("./src/routes/profileRoutes"));
+app.use("/group", require("./src/routes/groupRoutes"));
+app.use("/schedule", require("./src/routes/scheduleRoutes"));
 
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
-const mongoURI = process.env.DB_CONN
-
+const mongoURI = process.env.DB_CONN;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 
   // Connect to MongoDB
-  mongoose.connect(mongoURI, {
-    
-  })
-  .then(() => {
-    console.log('Connected to MongoDB successfully');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+  mongoose
+    .connect(mongoURI, {})
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
 });
