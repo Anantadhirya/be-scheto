658d9e6bdfa7dcbde1b96811c6106ee5cc48a8f7
Anantadhirya <anantadhirya@gmail.com>
2024-11-26 19:11:18 +0700

    fix: deployment

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ dotenv.config();
 const app = express();
 
 const corsConfig = require("./src/config/corsConfig");
-app.options("*", cors({ origin: true, credentials: true }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors(corsConfig));
 
 // Middleware
 app.use(express.json());
